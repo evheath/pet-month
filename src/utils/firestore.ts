0b8d0316cae9ee5306e4c1edd4e4165459b0bcce
evheath <elliot.v.heath@gmail.com>
2022-07-09 12:31:26 -0500

    service account through ENV

--- src/utils/firestore.ts
+++ src/utils/firestore.ts
@@ -1,9 +1,13 @@
-import { PetData } from '@/utils/PetData';
-import { initializeApp, getApp, getApps } from 'firebase-admin/app';
-const app = getApps().length === 0 ? initializeApp() : getApp();
+// firestore boilerplate
+import { initializeApp, getApp, getApps, cert } from 'firebase-admin/app';
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string
+);
+const app = getApps().length === 0 ? initializeApp({ credential: cert(serviceAccount) }) : getApp();
 import { getFirestore } from 'firebase-admin/firestore';
 const db = getFirestore(app);
 
+import { PetData } from '@/utils/PetData';
 export async function updatePetData(numPets: number) {
   console.log(`updating pet data for ${numPets} pets`);
   const index = numPets - 1;
