e06ccc223ddc90ce7f6f764e5a4fb24686643982
evheath <elliot.v.heath@gmail.com>
2022-07-09 09:15:54 -0500

    coloring index of interest

---
+++ src/components/PetGraph.tsx
@@ -0,0 +1,32 @@
+import { PetData } from '@/utils/PetData';
+import { BarChart, Bar, XAxis, YAxis, ResponsiveContainer, Cell } from 'recharts';
+
+export function PetGraph({ data, slug }: { data: PetData[], slug: string }) {
+  const slugInt = parseInt(slug);
+  const indexOfInterest: number = slugInt > 6 ? 5 : slugInt - 1
+  return (
+    <ResponsiveContainer width="100%" height="100%">
+      <BarChart
+        width={500}
+        height={300}
+        data={data}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Bar dataKey="families">
+          {
+            data.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={index === indexOfInterest ? "#82ca9d" : "#8884d8"} />
+            ))
+          }
+        </Bar>
+      </BarChart>
+    </ResponsiveContainer>
+  );
+}
--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,18 +1,47 @@
 import type { InferGetServerSidePropsType, NextPage } from "next";
 import Head from "next/head";
-import { Histogram } from "@/components/Histogram";
-
+import { PetGraph } from "@/components/PetGraph";
 import { GetServerSideProps } from 'next'
+import { PetData } from "@/utils/PetData";
+
+
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const data: PetData[] = [
+    {
+      name: '1 Pet',
+      families: 2400,
+    },
+    {
+      name: '2 Pets',
+      families: 1398,
+    },
+    {
+      name: '3 Pets',
+      families: 9800,
+    },
+    {
+      name: '4 Pets',
+      families: 3908,
+    },
+    {
+      name: '5 Pets',
+      families: 4800,
+    },
+    {
+      name: '6+ Pets',
+      families: 3800,
+    },
+  ];
   return {
     props: {
       slug: params!.slug as string,
+      data
     }
   }
 }
 
-const Home: NextPage = ({ slug }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+const Home: NextPage = ({ slug, data }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   return (
     <>
       <Head>
@@ -20,10 +49,11 @@ const Home: NextPage = ({ slug }: InferGetServerSidePropsType<typeof getServerSi
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="h-screen flex flex-col items-center justify-center">
-        <h1 className="bg-black text-white">Check out the stats</h1>
-        <p>Looking at the stats for people with {slug} pets</p>
+        {/* <h1 className="bg-black text-white">Check out the stats</h1> */}
+        {/* <p>Looking at the stats for people with {slug} pets</p> */}
 
-        <Histogram />
+        <PetGraph data={data} slug={slug} />
+
       </main>
     </>
   );
