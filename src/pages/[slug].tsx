b78f1bf240b54c97d75cb1faef19f6cdc3639e1f
evheath <elliot.v.heath@gmail.com>
2022-07-09 08:10:50 -0500

    displaying a chart, passing slug

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,15 +1,29 @@
-import type { NextPage } from "next";
+import type { InferGetServerSidePropsType, NextPage } from "next";
 import Head from "next/head";
+import { Histogram } from "@/components/Histogram";
 
-const Home: NextPage = (props) => {
+import { GetServerSideProps } from 'next'
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  return {
+    props: {
+      slug: params!.slug as string,
+    }
+  }
+}
+
+const Home: NextPage = ({ slug }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   return (
     <>
       <Head>
         <title>Pet Month</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
+      <main className="h-screen flex flex-col items-center justify-center">
         <h1 className="bg-black text-white">Check out the stats</h1>
+        <p>Looking at the stats for people with {slug} pets</p>
+
+        <Histogram />
       </main>
     </>
   );
